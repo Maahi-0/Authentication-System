6ba1dfc787a784094ba49c7444aca123a6573e95
Muskan <muskanwagh1608@gmail.com>
2026-03-24 23:16:22 +0530

    new commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const home = () => {
-    return (
-        <div>
-            contact page
-        </div>
-    )
-}
+  return <div>contact page is here</div>;
+};
 
-export default home
+export default home;
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const home = () => {
-    return (
-        <div>
-            home page
-        </div>
-    )
-}
+  return <div>home page is here</div>;
+};
 
-export default home
+export default home;
